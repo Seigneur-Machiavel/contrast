a736852c79b7d8cf195ab22e0f2163c88f9deb15
Seigneur-Machiavel <guibi50ae@hotmail.fr>
2025-01-14 10:20:43 +0100

    fix: comment out stream closure logic to prevent premature termination during sync handling

--- node/src/sync-handler.mjs
+++ node/src/sync-handler.mjs
@@ -62,11 +62,11 @@ export class SyncHandler {
             const serializedResponse = serializer.serialize.rawData(response);
             await lp.write(serializedResponse);
 
-            while (stream.writeStatus === 'writing') { await new Promise(resolve => setTimeout(resolve, 100)); }
-            this.miniLogger.log(`-----> Closing stream with peer ${peerIdStr}`, (m) => { console.debug(m); });
+            //while (stream.writeStatus === 'writing') { await new Promise(resolve => setTimeout(resolve, 100)); }
+            //this.miniLogger.log(`-----> Closing stream with peer ${peerIdStr}`, (m) => { console.debug(m); });
             //await stream.close();
 
-            if (stream.status === 'closed') { return; }
+            //if (stream.status === 'closed') { return; }
             //await stream.close();
         } catch (err) {
             if (err.code === 'ABORT_ERR') { return; }
