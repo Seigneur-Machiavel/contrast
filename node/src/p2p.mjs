059f1d527c98ff11e823830cc246b6359084f1f8
Seigneur-Machiavel <guibi50ae@hotmail.fr>
2025-01-14 13:07:20 +0100

    fix: ensure proper stream closure in P2PNetwork after message write and response read

--- node/src/p2p.mjs
+++ node/src/p2p.mjs
@@ -296,14 +296,15 @@ class P2PNetwork extends EventEmitter {
 
             const serialized = serializer.serialize.rawData(message);
             await lp.write(serialized);
-            //await stream.closeWrite();
             this.miniLogger.log(`Message written to stream (${serialized.length} bytes)`, (m) => { console.info(m); });
+            await stream.closeWrite();
 
             const res = await lp.read();
             if (!res) { miniLogger.log(`No response received`, (m) => { console.error(m); }); return false; }
-            //await stream.closeRead();
-            
             this.miniLogger.log(`Response read from stream (${res.length} bytes)`, (m) => { console.info(m); });
+            //await stream.closeRead();
+            await stream.close()
+            
             
             //const rstatus = stream.readStatus;
             //const wstatus = stream.writeStatus;
