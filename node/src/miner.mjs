d69c17b7e70e445f5a023f15772a44b0b4bf6475
Seigneur-Machiavel <guibi50ae@hotmail.fr>
2025-01-28 12:58:51 +0100

    fix: refine block candidate validation logic; ensure proper reward comparison and difficulty checks

--- node/src/miner.mjs
+++ node/src/miner.mjs
@@ -64,8 +64,6 @@ export class Miner {
 
         const prevHash = this.node.blockchain.lastBlock ? this.node.blockchain.lastBlock.hash : '0000000000000000000000000000000000000000000000000000000000000000';
         if (blockCandidate.prevHash !== prevHash) { return false; }
-
-        const betterReward = posReward > this.bestCandidate.Txs[0].outputs[0].amount;
         
         let reasonChange = 'none';
         if (!this.bestCandidate) {
@@ -78,7 +76,8 @@ export class Miner {
             const newCandidateFinalDiff = mining.getBlockFinalDifficulty(blockCandidate).finalDifficulty;
             const bestCandidateFinalDiff = mining.getBlockFinalDifficulty(this.bestCandidate).finalDifficulty;
             if (newCandidateFinalDiff < bestCandidateFinalDiff) { reasonChange = `(easier block: ${newCandidateFinalDiff} < ${bestCandidateFinalDiff})`; }
-            if (newCandidateFinalDiff === bestCandidateFinalDiff && betterReward) { reasonChange = `(higher posReward: ${posReward} > ${this.bestCandidate.Txs[0].outputs[0].amount})`; }
+            if (newCandidateFinalDiff !== bestCandidateFinalDiff) { return false; }
+            if (posReward > this.bestCandidate.Txs[0].outputs[0].amount) { reasonChange = `(higher posReward: ${posReward} > ${this.bestCandidate.Txs[0].outputs[0].amount})`; }
         }
         if (reasonChange === 'none') { return false; }
 
