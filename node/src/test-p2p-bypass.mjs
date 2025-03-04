fc596f228c848386088b988a16660e6f3c13325f
Seigneur-Machiavel <guibi50ae@hotmail.fr>
2025-03-04 14:56:06 +0100

    fix: update bootstrap node addresses to remove unnecessary protocols

--- node/src/test-p2p-bypass.mjs
+++ node/src/test-p2p-bypass.mjs
@@ -39,7 +39,8 @@ const node = await createLibp2p({
 })
 await node.start();
 
-const target = '/dns4/contrast.observer/tcp/27260';
+//const target = '/dns4/contrast.observer/tcp/27260'; // contrast.observer direct IP
+const target = '/dns4/contrast.observer/tcp/27260'; // contrast.observer DNS
 //const target = '/ip4/192.168.4.23/tcp/61121/ws/p2p/12D3KooWRwDMmqPkdxg2yPkuiW1gPCgcdHGJtyaGfxdgAuEpNzD7';
 //const target = '/ip4/141.8.119.6/tcp/46124'
 //const target = '/ip4/193.43.70.41/tcp/1603/p2p/12D3KooWRwDMmqPkdxg2yPkuiW1gPCgcdHGJtyaGfxdgAuEpNzD7' // YOGA
