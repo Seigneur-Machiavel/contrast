5d78493716be8413d9098d53453490c74b9a7017
Seigneur-Machiavel <guibi50ae@hotmail.fr>
2025-03-04 14:51:52 +0100

    fix: update P2P node configuration with additional transports and services

--- node/src/test-p2p-bypass.mjs
+++ node/src/test-p2p-bypass.mjs
@@ -4,7 +4,11 @@ import { identify } from '@libp2p/identify';
 import { peerIdFromString } from '@libp2p/peer-id';
 
 import { kadDHT } from '@libp2p/kad-dht';
+import { uPnPNAT } from '@libp2p/upnp-nat';
+import { mdns } from '@libp2p/mdns';
 import { autoNAT } from '@libp2p/autonat';
+import { circuitRelayTransport, circuitRelayServer } from '@libp2p/circuit-relay-v2';
+import { webRTCDirect, webRTC } from '@libp2p/webrtc';
 import { multiaddr } from '@multiformats/multiaddr';
 import { createLibp2p } from 'libp2p';
 import { dcutr } from '@libp2p/dcutr';
@@ -19,25 +23,26 @@ const dhtService = kadDHT({ enabled: true, randomWalk: true });
 const node = await createLibp2p({
 	connectionGater: { denyDialMultiaddr: () => false },
 	privateKey: privateKeyObject,
-	addresses: { listen: ['/p2p-circuit', '/ip4/0.0.0.0/tcp/0/ws'] }, // '/webrtc-direct'
-	transports: [tcp(), webSockets()],
+	addresses: { listen: ['/p2p-circuit', '/ip4/0.0.0.0/tcp/0', '/ip4/0.0.0.0/tcp/0/ws', '/webrtc-direct'] }, // '/webrtc-direct'
+	transports: [circuitRelayTransport({ discoverRelays: 3 }), tcp(), webSockets(), webRTCDirect()],
 	connectionEncrypters: [noise()],
 	streamMuxers: [yamux()],
 	services: {
 		identify: identify(),
 		dht: dhtService,
 		dcutr: dcutr(),
+		upnp: uPnPNAT(),
 		autoNAT: autoNAT(),
-		//circuitRelay: circuitRelayServer({ reservations: { maxReservations: 24, reservationTtl: 60_000 } })
+		circuitRelay: circuitRelayServer({ reservations: { maxReservations: 24, reservationTtl: 60_000 } })
 	},
-	peerDiscovery: [dhtService]
+	peerDiscovery: [mdns(), dhtService]
 })
 await node.start();
 
-//const target = '/dns4/contrast.observer/tcp/27260/ws/p2p/12D3KooWEKjHKUrLW8o8EAL9wofj2LvWynFQZzx1kLPYicd4aEBX';
-
+const target = '/dns4/contrast.observer/tcp/27260';
 //const target = '/ip4/192.168.4.23/tcp/61121/ws/p2p/12D3KooWRwDMmqPkdxg2yPkuiW1gPCgcdHGJtyaGfxdgAuEpNzD7';
-const target = '/ip4/141.8.119.6/tcp/46124/'
+//const target = '/ip4/141.8.119.6/tcp/46124'
+//const target = '/ip4/193.43.70.41/tcp/1603/p2p/12D3KooWRwDMmqPkdxg2yPkuiW1gPCgcdHGJtyaGfxdgAuEpNzD7' // YOGA
 const multiAddr = multiaddr(target);
 try {
     await node.dial(multiAddr, { signal: AbortSignal.timeout(3000) })
